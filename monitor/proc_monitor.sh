8b75b403a8af5eb979ea4d6fd696a39b6d23b77d
guhan121 <guhan121@qq.com>
2018-12-10 15:09:55 +0800

    Update proc_monitor.sh

--- monitor/proc_monitor.sh
+++ monitor/proc_monitor.sh
@@ -28,8 +28,8 @@ process_cpu() {
 
 process_cpu_rate() {
 	# processCPUUse = processTime / totalCPUTime
-	processTime=`expr $4 - $2`
-	totalCPUTime=`expr $3 - $1`
+	processTime=$(( $4 - $2 ))
+	totalCPUTime=$(($3 - $1))
 	rate=`awk 'BEGIN{printf "%.1f",('$processTime'/'$totalCPUTime')*100}'`
 	//rate=`expr "scale=2;$processTime * 100 / $totalCPUTime" | bc -l`
 	echo $rate
