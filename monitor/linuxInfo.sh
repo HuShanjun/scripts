84bd3cb2ad7d65ba8b568787b2fd5b0b7656d2fe
guhan121 <guhan121@qq.com>
2018-12-26 10:18:26 +0800

    Update linuxInfo.sh

--- monitor/linuxInfo.sh
+++ monitor/linuxInfo.sh
@@ -2,7 +2,8 @@
 
 v1=$(cat /proc/cpuinfo |grep "physical id"|sort |uniq|wc -l)
 v2=$(cat /proc/cpuinfo |grep "processor"|wc -l  )
-v3=$(cat /proc/cpuinfo |grep "cores"|uniq)
+v3=$(cat /proc/cpuinfo |grep "cores"|uniq|cut -d ":" -f2)
 v4=$(free -g|grep Mem|awk '{print $2}';)
 
-echo "$v1physical cpu, $v2 cores, $v3 processor ; total mem:$v4G"
+echo "$v1 physical cpu, $v3 cores, $v2 processor ; total mem:${v4}G"
+
